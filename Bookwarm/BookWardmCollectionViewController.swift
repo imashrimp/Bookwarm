5e3f9e7224c3370c8cd4fc7eef69f710146176b3
imashrimp <kwonhs8287@gmail.com>
2023-07-31 17:29:34 +0900

    set collectionView

---
+++ Bookwarm/BookWardmCollectionViewController.swift
@@ -0,0 +1,47 @@
+//
+//  BookWardmCollectionViewController.swift
+//  Bookwarm
+//
+//  Created by 권현석 on 2023/07/31.
+//
+
+import UIKit
+
+class BookWardmCollectionViewController: UICollectionViewController {
+    
+    let movieList = MovieList()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let nib = UINib(nibName: "BookWarmCollectionViewCell", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: "BookWarmCollectionViewCell")
+
+        setCollectionViewLayout()
+    }
+    
+    func setCollectionViewLayout() {
+        let layout = UICollectionViewFlowLayout()
+        let spacing: CGFloat = 20
+        let width = (UIScreen.main.bounds.width - ( spacing * 3)) / 2
+        
+        layout.itemSize = CGSize(width: width, height: width)
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        collectionView.collectionViewLayout = layout
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movieList.movie.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BookWarmCollectionViewCell", for: indexPath) as! BookWarmCollectionViewCell
+        
+        cell.configureCellAttribute()
+        cell.setUIContents(movie: movieList.movie[indexPath.row])
+        
+        return cell
+    }
+}
---
+++ Bookwarm/BookWarmCollectionViewCell.swift
@@ -0,0 +1,44 @@
+//
+//  BookWarmCollectionViewCell.swift
+//  Bookwarm
+//
+//  Created by 권현석 on 2023/07/31.
+//
+
+import UIKit
+
+class BookWarmCollectionViewCell: UICollectionViewCell {
+    
+    var randomColor: UIColor {
+        UIColor(red: .random(in: 0...1),
+                green: .random(in: 0...1),
+                blue: .random(in: 0...1),
+                alpha: 1)
+    }
+    
+    @IBOutlet var movieTitleLabel: UILabel!
+    @IBOutlet var ratingLabel: UILabel!
+    @IBOutlet var posterImageView: UIImageView!
+    
+    
+    func configureCellAttribute() {
+        self.layer.cornerRadius = 10
+        self.backgroundColor = randomColor
+        
+        movieTitleLabel.textColor = .white
+        movieTitleLabel.font = .systemFont(ofSize: 20, weight: .bold)
+        
+        ratingLabel.textColor = .white
+        ratingLabel.font = .systemFont(ofSize: 20)
+        
+        posterImageView.contentMode = .scaleAspectFit
+    }
+    
+    func setUIContents(movie: Movie) {
+        movieTitleLabel.text = movie.title
+        ratingLabel.text = "\(movie.rate)점"
+        posterImageView.image = UIImage(named: movie.title)
+    }
+    
+    
+}
